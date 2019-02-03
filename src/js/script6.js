6dd9b0ec09bcd7da0843e70a6ba65bc9e9055fc9
Ali-X <alina240895@yandex.ru>
2019-02-03 21:43:06 +0200

    Implement `product per page` input

--- src/js/script6.js
+++ src/js/script6.js
@@ -1,24 +1,36 @@
 ;(function() {
-  let rowElements = document.querySelectorAll('tbody > tr');
-  let rowsPerPage = parseInt(document.getElementsByName('rowsPerPage')[0].getAttribute('value'));
-  let filterCountry = document.getElementsByClassName('filter__input')[0].getAttribute('value');
+  let rowsPerPageElement = document.getElementsByName('rowsPerPage')[0];
+  rowsPerPageElement.oninput = updateTable;
+  rowsPerPageElement.min = 1;
+  rowsPerPageElement.max = document.querySelectorAll('tbody > tr').length;
 
-  let selectedPage = document.getElementsByClassName('pager__page--current')[0].textContent;
-  let pageInfoElement = document.getElementsByClassName('pager__info')[0];
 
-  rowElements.forEach(function(elem, num) {
-    if (!(elem.getElementsByClassName('country')[0].textContent.startsWith(filterCountry))) {
-      elem.style.display = 'none';
-    }
-  });
+  function updateTable() {
+    let rowElements = document.querySelectorAll('tbody > tr');
 
-  rowElements.forEach(function(elem, num) {
-    if (!(num >= (selectedPage * rowsPerPage) && num <= (selectedPage * rowsPerPage + rowsPerPage))) {
-      elem.style.display = 'none';
-    } else {
-      elem.getElementsByClassName('position')[0].textContent = num;
-    }
-  });
+    let rowsPerPage = parseInt(document.getElementsByName('rowsPerPage')[0].value);
+    let filterCountry = document.getElementsByClassName('filter__input')[0].value;
 
-  pageInfoElement.textContent = 'Show ' + selectedPage * rowsPerPage + ' to ' + (selectedPage * rowsPerPage + rowsPerPage) + ' of ' + rowElements.length + ' rows';
+    let selectedPage = document.getElementsByClassName('pager__page--current')[0].textContent;
+    let pageInfoElement = document.getElementsByClassName('pager__info')[0];
+
+    rowElements.forEach(function(elem, num) {
+      if (!(elem.getElementsByClassName('country')[0].textContent.startsWith(filterCountry))) {
+        elem.style.display = 'none';
+      } else {
+        elem.style.display = 'table-row';
+      }
+    });
+
+    rowElements.forEach(function(elem, num) {
+      if (!(num >= (selectedPage * rowsPerPage) && num < (selectedPage * rowsPerPage + rowsPerPage))) {
+        elem.style.display = 'none';
+      } else {
+        elem.style.display = 'table-row';
+        elem.getElementsByClassName('position')[0].textContent = num;
+      }
+    });
+
+    pageInfoElement.textContent = 'Show ' + selectedPage * rowsPerPage + ' to ' + (selectedPage * rowsPerPage + rowsPerPage) + ' of ' + rowElements.length + ' rows';
+  }
 })();
